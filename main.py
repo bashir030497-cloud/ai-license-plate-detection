6bb816eeb05642636f92ab33a5f858c291692738
Bashir Ahmad <bashir030497@gmail.com>
2026-07-27 04:25:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ model = YOLO("best.pt")
 
 # ---- Fetch TURN credentials from Metered ----
 APP_NAME = "ai-license-plate"      # apna app name
-API_KEY = "gSsn5D6GYxefidkUJeEHS7qsvcre9MzepRYOl1KTzC0myaRE"           # apni API key yahan daalo
+API_KEY = "/api/v1/turn/credentials?apiKey=<YOUR_API_KEY>"           # apni API key yahan daalo
 
 @st.cache_resource
 def get_ice_servers():
